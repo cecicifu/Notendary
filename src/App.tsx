fb4905fab2f99c04c495bd931c1da90c1f9f7f0f
cecicifu <ceciliocifuentes@gmail.com>
2022-08-19 21:09:55 +0200

    ✨ Temp DB connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import {
   SafeAreaView,
   ScrollView,
@@ -9,61 +9,101 @@ import {
 import {Calendar} from 'react-native-calendars'
 import {Text, Avatar} from '@rneui/themed'
 import AddFloatingButton from './components/AddFloatingButton'
+import {
+  createTable,
+  getDBConnection,
+  getNotes,
+  saveNote,
+} from './services/db.service'
+import {Note} from './models/note.type'
 
-const App = () => (
-  <SafeAreaView>
-    <StatusBar />
-    <ScrollView
-      contentContainerStyle={{height: '100%'}}
-      contentInsetAdjustmentBehavior="automatic">
-      <View style={styles.defaultView}>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            marginTop: 10,
-          }}>
-          <Text style={{fontSize: 45, marginLeft: 10}}>Notendary</Text>
-          <Avatar
-            size="medium"
-            rounded
-            title="CC"
-            onPress={() => console.log('Profile')}
-            containerStyle={{marginRight: 10, backgroundColor: '#83AF9B'}}
+const App = () => {
+  const [notes, setNotes] = useState<Note[] | null>(null)
+
+  const initDB = useCallback(async () => {
+    try {
+      const initNotes: Note[] = [
+        {
+          id: 0,
+          title: 'first note',
+          description: 'the description',
+          datetime: new Date().toISOString(),
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString(),
+        },
+      ]
+      const db = await getDBConnection()
+      await createTable(db)
+      const storeNotes = await getNotes(db)
+
+      if (storeNotes.length) return setNotes(storeNotes)
+
+      await saveNote(db, initNotes)
+      setNotes(initNotes)
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
+
+  useEffect(() => {
+    initDB()
+  }, [initDB])
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView
+        contentContainerStyle={{height: '100%'}}
+        contentInsetAdjustmentBehavior="automatic">
+        <View style={styles.defaultView}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: 10,
+            }}>
+            <Text style={{fontSize: 45, marginLeft: 10}}>Notendary</Text>
+            <Avatar
+              size="medium"
+              rounded
+              title="CC"
+              onPress={() => console.log('Profile')}
+              containerStyle={{marginRight: 10, backgroundColor: '#83AF9B'}}
+            />
+          </View>
+
+          <Calendar
+            theme={{
+              calendarBackground: '#ffffff',
+              todayTextColor: '#FE4365',
+              arrowColor: '#FE4365',
+            }}
+            onDayPress={day => {
+              console.log('selected day', day)
+            }}
+            monthFormat={'MMMM - yyyy'}
+            onMonthChange={month => {
+              console.log('month changed', month)
+            }}
+            firstDay={1}
+            onPressArrowLeft={subtractMonth => subtractMonth()}
+            onPressArrowRight={addMonth => addMonth()}
+            disableAllTouchEventsForDisabledDays={true}
+            enableSwipeMonths={true}
+          />
+          <Text style={styles.infoText}>
+            You can add new events clicking on a day or using the "+" button
+          </Text>
+          <AddFloatingButton
+            eventAction={() => console.log('Add event')}
+            categoryAction={() => console.log('Add event')}
           />
         </View>
-
-        <Calendar
-          theme={{
-            calendarBackground: '#ffffff',
-            todayTextColor: '#FE4365',
-            arrowColor: '#FE4365',
-          }}
-          onDayPress={day => {
-            console.log('selected day', day)
-          }}
-          monthFormat={'MMMM - yyyy'}
-          onMonthChange={month => {
-            console.log('month changed', month)
-          }}
-          firstDay={1}
-          onPressArrowLeft={subtractMonth => subtractMonth()}
-          onPressArrowRight={addMonth => addMonth()}
-          disableAllTouchEventsForDisabledDays={true}
-          enableSwipeMonths={true}
-        />
-        <Text style={styles.infoText}>
-          You can add new events clicking on a day or using the "+" button
-        </Text>
-        <AddFloatingButton
-          eventAction={() => console.log('Add event')}
-          categoryAction={() => console.log('Add event')}
-        />
-      </View>
-    </ScrollView>
-  </SafeAreaView>
-)
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
   defaultView: {
