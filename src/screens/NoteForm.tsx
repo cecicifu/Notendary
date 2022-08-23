e3215b1afef4be416c8ca9d6593f99bcd30b6e0b
Ceci Cifu <15237067+cecicifu@users.noreply.github.com>
2022-08-23 20:03:37 +0200

    Feature/navigation (#2)

---
+++ src/screens/NoteForm.tsx
@@ -0,0 +1,19 @@
+import {styles} from '../styles/global'
+import {Text, Button} from '@rneui/themed'
+import {View} from 'react-native'
+import React from 'react'
+import type {NativeStackScreenProps} from '@react-navigation/native-stack'
+import type {RoutesStackParamList} from '../App'
+
+const NoteForm = ({
+  navigation,
+}: NativeStackScreenProps<RoutesStackParamList>) => {
+  return (
+    <View style={styles.defaultView}>
+      <Text>holaa</Text>
+      <Button title="Ir atras" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+export default NoteForm
---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  defaultView: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+  },
+  infoText: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 30,
+    width: 300,
+    display: 'flex',
+    alignSelf: 'center',
+  },
+})
