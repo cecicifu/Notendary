5cc4dba93a4569486f5e0ed2d231cce359f8a3b2
cecicifu <ceciliocifuentes@gmail.com>
2022-08-31 21:33:33 +0200

    ✨ note form

--- src/screens/NoteForm.tsx
+++ src/screens/NoteForm.tsx
@@ -1,5 +1,5 @@
 import {styles} from '../styles/global'
-import {Text, Button} from '@rneui/themed'
+import {Text, Button, Input} from '@rneui/themed'
 import {View} from 'react-native'
 import React from 'react'
 import type {NativeStackScreenProps} from '@react-navigation/native-stack'
@@ -10,8 +10,23 @@ const NoteForm = ({
 }: NativeStackScreenProps<RoutesStackParamList>) => {
   return (
     <View style={styles.defaultView}>
-      <Text>holaa</Text>
-      <Button title="Ir atras" onPress={() => navigation.goBack()} />
+      <Text h3 h3Style={{fontFamily: 'sans-serif-thin'}} style={{marginTop: 10, textAlign: 'center'}}>
+        New note
+      </Text>
+      <Input placeholder="Title" />
+      <Input numberOfLines={2} multiline placeholder="Description" />
+      <Input placeholder="Date" />
+      <View style={styles.buttonsInline}>
+        <Button style={styles.button} title="Save" color="#83AF9B" size="lg" />
+        <Button
+          style={styles.button}
+          color="#FE4365"
+          containerStyle={{marginLeft: 30}}
+          size="lg"
+          title="Cancel"
+          onPress={() => navigation.goBack()}
+        />
+      </View>
     </View>
   )
 }
