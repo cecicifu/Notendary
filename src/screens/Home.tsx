03301781d49e62525a9958548d90af5ebba9916f
cecicifu <ceciliocifuentes@gmail.com>
2022-09-02 21:36:29 +0200

    ✨ Logic to show the notes by selected day & delete each one

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,15 @@
+import {Avatar, Text, Dialog, Button, Divider} from '@rneui/themed'
+import {Calendar, DateData} from 'react-native-calendars'
 import {CalendarMark, notesToCalendarMark} from '../mappers/noteMapper'
-import {Avatar, Text} from '@rneui/themed'
-import {Calendar} from 'react-native-calendars'
-import {getAllNotes} from '../services/notes.service'
+import {
+  deleteNote,
+  getAllNotes,
+  getAllNotesByDate,
+} from '../services/notes.service'
+import {Modal, View} from 'react-native'
+import {Note} from '../models/Note'
 import {styles} from '../styles/global'
 import {useFocusEffect} from '@react-navigation/native'
-import {View} from 'react-native'
 import AddFloatingButton from '../components/AddFloatingButton'
 import React, {useCallback, useState} from 'react'
 import type {NativeStackScreenProps} from '@react-navigation/native-stack'
@@ -12,14 +17,43 @@ import type {RoutesStackParamList} from '../navigation/AppNavigation'
 
 const Home = ({navigation}: NativeStackScreenProps<RoutesStackParamList>) => {
   const [notes, setNotes] = useState<CalendarMark | null>(null)
+  const [modalVisible, setModalVisible] = useState(false)
+  const [notesFromSelectedDay, setNotesFromSelectedDay] = useState<
+    Note[] | null
+  >(null)
 
   useFocusEffect(
     useCallback(() => {
-      getAllNotes().then(notes => {
-        if (notes.length) setNotes(notesToCalendarMark(notes))
-      })
+      getAllNotesHandler()
     }, []),
   )
+
+  const getAllNotesHandler = async () => {
+    const allNotes = await getAllNotes()
+    if (allNotes.length) setNotes(notesToCalendarMark(allNotes))
+  }
+
+  const selectedDayHandler = async (day: DateData) => {
+    const notes = await getAllNotesByDate(day.dateString)
+    if (!notes.length) return
+
+    setModalVisible(true)
+    setNotesFromSelectedDay(notes)
+  }
+
+  const deleteNoteHandler = async (note: Note) => {
+    await deleteNote(note.id)
+
+    const newNotes = notesFromSelectedDay?.filter(
+      noteFromSelectedDay => noteFromSelectedDay.id !== note.id,
+    )
+
+    getAllNotesHandler()
+
+    if (newNotes?.length) return setNotesFromSelectedDay(newNotes)
+
+    setModalVisible(false)
+  }
 
   return (
     <View style={styles.defaultView}>
@@ -53,9 +87,7 @@ const Home = ({navigation}: NativeStackScreenProps<RoutesStackParamList>) => {
           todayTextColor: '#FE4365',
           arrowColor: '#FE4365',
         }}
-        onDayPress={day => {
-          console.log('selected day', day)
-        }}
+        onDayPress={day => selectedDayHandler(day)}
         monthFormat={'MMMM - yyyy'}
         onMonthChange={month => {
           console.log('month changed', month)
@@ -74,6 +106,52 @@ const Home = ({navigation}: NativeStackScreenProps<RoutesStackParamList>) => {
         noteAction={() => navigation.navigate('NoteForm')}
         categoryAction={() => console.log('Add category')}
       />
+      <Dialog
+        ModalComponent={Modal}
+        isVisible={modalVisible}
+        onPressOut={() => setModalVisible(false)}>
+        {notesFromSelectedDay && (
+          <>
+            <View style={{marginBottom: 16}}>
+              <Dialog.Title
+                title={new Date(
+                  notesFromSelectedDay[0].datetime,
+                ).toLocaleDateString()}
+              />
+              <Divider />
+            </View>
+            {notesFromSelectedDay.map(note => {
+              return (
+                <View key={note.id} style={{marginBottom: 16}}>
+                  <View
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                    }}>
+                    <View style={{display: 'flex', flexDirection: 'row'}}>
+                      <Text style={{fontWeight: 'bold'}}>
+                        {new Date(note.datetime).toLocaleTimeString()}
+                      </Text>
+                      <Text style={{marginHorizontal: 8}}>|</Text>
+                      <Text>{note.title}</Text>
+                    </View>
+                    <Button
+                      size="sm"
+                      type="outline"
+                      titleStyle={{color: 'red'}}
+                      buttonStyle={{borderColor: 'red'}}
+                      onPress={() => deleteNoteHandler(note)}>
+                      Delete
+                    </Button>
+                  </View>
+                  <Text>{note.description}</Text>
+                </View>
+              )
+            })}
+          </>
+        )}
+      </Dialog>
     </View>
   )
 }
