74186e8915c310129a2a1b357677814313cec086
Ceci Cifu <15237067+cecicifu@users.noreply.github.com>
2022-09-24 21:52:47 +0200

    🐛 New helper function to fix timezone (#9)

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -13,5 +13,31 @@ export const getDeviceLocale = () => {
         NativeModules.SettingsManager.settings.AppleLanguages[0] // iOS 13
       : NativeModules.I18nManager.localeIdentifier
 
-  return language.replace("_", "-")
+  return language.replace('_', '-')
+}
+
+export const toIsoString = (date: Date) => {
+  const tzo = -date.getTimezoneOffset(),
+    dif = tzo >= 0 ? '+' : '-',
+    pad = function (num: number) {
+      return (num < 10 ? '0' : '') + num
+    }
+
+  return (
+    date.getFullYear() +
+    '-' +
+    pad(date.getMonth() + 1) +
+    '-' +
+    pad(date.getDate()) +
+    'T' +
+    pad(date.getHours()) +
+    ':' +
+    pad(date.getMinutes()) +
+    ':' +
+    pad(date.getSeconds()) +
+    dif +
+    pad(Math.floor(Math.abs(tzo) / 60)) +
+    ':' +
+    pad(Math.abs(tzo) % 60)
+  )
 }
